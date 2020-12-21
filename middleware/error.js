dce0bd9c9647ba7825b354d39e2186a2967efea1
Fairouz  West <fairouzkoehlerwest@gmail.com>
2020-12-21 15:34:16 +0100

    adds field validation

--- middleware/error.js
+++ middleware/error.js
@@ -5,13 +5,26 @@ const errorHandler = (err, req, res, next) => {
 
   error.message = err.message;
 
-  // Log to console for de
-  console.log(err.stack.red);
+  // Log to console for dev
+  console.log(err);
 
   // Mongoose bad ObjectID
   if (err.name === "CastError") {
     const message = `Bootcamps not found with id of ${error.value}`;
     error = new ErrorResponse(message, 404);
+  }
+
+  // Mongoose duplicate key
+  if (err.code === 11000) {
+    const message = "Duplicate field value entered";
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Mongoose validation error
+
+  if (err.name === "ValidationError") {
+    const message = Object.values(error.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
   }
 
   res.status(error.statusCode || 500).json({
