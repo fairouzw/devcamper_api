f870300a4497594ab10e156b5f0a2fe0b62acfaf
Fairouz  West <fairouzkoehlerwest@gmail.com>
2020-12-22 12:11:12 +0100

    implements GeoJSON location and Geocoder Hooks

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
+const { geocode } = require("../utils/geocoder");
+const geocoder = require("../utils/geocoder");
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -38,6 +40,7 @@ const BootcampSchema = new mongoose.Schema({
     required: [true, "Please add an address"],
   },
   location: {
+    // GeoJSON Point
     type: {
       type: String,
       enum: ["Point"],
@@ -105,4 +108,23 @@ BootcampSchema.pre("save", function (next) {
   next();
 });
 
+//Geocode & create location field
+BootcampSchema.pre("save", async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: "Point",
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode,
+  };
+
+  // Do not save address in DB
+  this.address = undefined;
+
+  next();
+});
 module.exports = mongoose.model("Bootcamps", BootcampSchema);
