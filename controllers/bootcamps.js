91cab6a065b36d8c32b42175591cf38bd5e65382
Fairouz  West <fairouzkoehlerwest@gmail.com>
2020-12-28 18:15:07 +0100

    adds crud actions for courses

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -29,10 +29,8 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     (match) => `$${match}`
   );
 
-  console.log(queryStr);
-
   // Finding resource
-  query = Bootcamp.find(JSON.parse(queryStr));
+  query = Bootcamp.find(JSON.parse(queryStr)).populate("courses");
 
   // Select fields
   if (req.query.select) {
@@ -137,12 +135,14 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 // @access Private
 
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+  const bootcamp = await Bootcamp.findById(req.params.id);
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Bootcamps not found with id of ${req.params.id}`, 404)
     );
   }
+
+  bootcamp.remove();
   res.status(200).json({ success: true, data: {} });
 });
 
