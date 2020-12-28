8190602f433ed285bc0fd84ebed39aed1f9e6fb0
Fairouz  West <fairouzkoehlerwest@gmail.com>
2020-12-28 15:58:26 +0100

    creates course modes & routes

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,6 +3,7 @@ const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/async");
 const geocoder = require("../utils/geocoder");
 const Bootcamp = require("../models/Bootcamp");
+
 // @desc Get all bootcamps
 // @route GET /api/vi/bootcamps
 // @access Public
--- server.js
+++ server.js
@@ -13,6 +13,7 @@ connectDB();
 
 //Route files
 const bootcamps = require("./routes/bootcamps");
+const courses = require("./routes/courses");
 
 const app = express();
 
@@ -25,8 +26,9 @@ if (process.env.NODE_ENV === "development") {
 }
 
 // Mount routers
-
 app.use("/api/v1/bootcamps", bootcamps);
+app.use("/api/v1/courses", courses);
+
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
