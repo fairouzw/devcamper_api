a8e31f93ecda8e0e6994fb97cdcc94fcadb7b40e
Fairouz  West <fairouzkoehlerwest@gmail.com>
2021-01-03 20:37:58 +0100

    user login

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ const asyncHandler = require("../middleware/async");
 const User = require("../models/User");
 
 // @desc Register user
-// @route GET /api/vi/auth/register
+// @route POST /api/vi/auth/register
 // @access Public
 
 exports.register = asyncHandler(async (req, res, next) => {
@@ -18,6 +18,41 @@ exports.register = asyncHandler(async (req, res, next) => {
   });
 
   // Create toke
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({
+    success: true,
+    token,
+  });
+});
+
+// @desc Login user
+// @route POST /api/vi/auth/login
+// @access Public
+
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Validate email and password
+  if (!email || !password) {
+    return next(new ErrorResponse("Please provide email and password", 400));
+  }
+
+  //Check for the user
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
+
+  // Check if password matches
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
+
+  // Create token
   const token = user.getSignedJwtToken();
 
   res.status(200).json({
