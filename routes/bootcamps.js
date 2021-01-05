7c3d6b6ce7bb3052367aad496b8e09a37ed0262d
Fairouz  West <fairouzkoehlerwest@gmail.com>
2021-01-05 23:09:04 +0100

    auth protect middleware

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -17,6 +17,7 @@ const advancedResults = require("../middleware/advancedResults");
 
 const courseRouter = require("./courses");
 
+const { protect } = require("../middleware/auth");
 const router = express.Router();
 
 // Re-route into other resource routers
@@ -24,17 +25,17 @@ router.use("/:bootcampId/courses", courseRouter);
 
 router.route("/radius/:zipcode/:distance").get(getBootcampsInRadius);
 
-router.route("/:id/photo").put(bootcampPhotoUpload);
+router.route("/:id/photo").put(protect, bootcampPhotoUpload);
 
 router
   .route("/")
   .get(advancedResults(Bootcamp, "courses"), getBootcamps)
-  .post(createBootcamp);
+  .post(protect, createBootcamp);
 
 router
   .route("/:id")
   .get(getBootcamp)
-  .put(updateBootcamp)
-  .delete(deleteBootcamp);
+  .put(protect, updateBootcamp)
+  .delete(protect, deleteBootcamp);
 
 module.exports = router;
