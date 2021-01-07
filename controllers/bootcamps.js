9c78937a418410d4a12ed7507c913a4413b230ed
Fairouz  West <fairouzkoehlerwest@gmail.com>
2021-01-07 20:29:39 +0100

    bootcamp and user relationship

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -34,7 +34,23 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 // @access Private
 
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
+  // Add user to req.body
+  req.body.user = req.user.id;
+
+  // Check for published bootcamp
+  const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id });
+
   const bootcamp = await Bootcamp.create(req.body);
+
+  // If the user is not an admin, they can only add one bootcamp
+  if (publishedBootcamp && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `The user with ID ${req.user.id} has already published a bootcamp`,
+        400
+      )
+    );
+  }
 
   res.status(201).json({
     success: true,
