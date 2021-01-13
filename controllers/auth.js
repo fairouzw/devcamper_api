15179273935bb7ed8d3e202ff5c413210d36019c
Fairouz  West <fairouzkoehlerwest@gmail.com>
2021-01-13 23:41:23 +0100

    update user details and password

--- controllers/auth.js
+++ controllers/auth.js
@@ -61,6 +61,23 @@ exports.getMe = asyncHandler(async (req, res, next) => {
   res.status(200).json({ success: true, data: user });
 });
 
+// @desc Update user details
+// @route PUT /api/vi/auth/updatedetails
+// @access Private
+
+exports.updateDetails = asyncHandler(async (req, res, next) => {
+  const fieldsToUpdate = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+  const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({ success: true, data: user });
+});
+
 // @desc reset password
 // @route PUT /api/vi/auth/resetpassword/:resettoken
 // @access Public
@@ -110,6 +127,24 @@ const sendTokenResponse = (user, statusCode, res) => {
     .cookie("token", token, options)
     .json({ success: true, token });
 };
+
+// @desc Update password
+// @route PUT /api/vi/auth/updatepassword
+// @access Private
+
+exports.updatePassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select("+password");
+
+  //Check current password
+  if (!(await user.matchPassword(req.body.currentPassword))) {
+    return next(new ErrorResponse("Password is incorrect", 401));
+  }
+
+  user.password = req.body.newPassword;
+  await user.save();
+
+  sendTokenResponse(user, 200, res);
+});
 
 // @desc Forgot password
 // @route POST /api/vi/auth/forgotpassword
