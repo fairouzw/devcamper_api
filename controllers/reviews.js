66a650f6c18d1df3ccebf7772080de8d88072bb7
Fairouz  West <fairouzkoehlerwest@gmail.com>
2021-01-15 16:13:46 +0100

    review Models

---
+++ controllers/reviews.js
@@ -0,0 +1,22 @@
+const ErrorResponse = require("../utils/errorResponse");
+const asyncHandler = require("../middleware/async");
+const Review = require("../models/Review");
+const Bootcamp = require("../models/Bootcamp");
+
+// @desc Get reviews
+// @route GET /api/vi/reviews
+// @route GET /api/vi/bootcamps/:bootcampId/revies
+// @access Public
+
+exports.getReviews = asyncHandler(async (req, res, next) => {
+  if (req.params.bootcampId) {
+    const reviews = await Review.find({ bootcamp: req.params.bootcampId });
+    return res.status(200).json({
+      success: true,
+      count: reviews.length,
+      data: reviews,
+    });
+  } else {
+    res.status(200).json(res.advancedResults);
+  }
+});
